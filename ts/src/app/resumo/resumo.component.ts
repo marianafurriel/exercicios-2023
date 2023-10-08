3a2328b3cda3e9bc73322509ea6069055fea7f25
Mariana Furriel <marianafurrielss@gmail.com>
2023-10-08 08:20:47 -0300

    feat: expansão e redução do resumo

--- ts/src/app/resumo/resumo.component.ts
+++ ts/src/app/resumo/resumo.component.ts
@@ -5,4 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './resumo.component.html',
   styleUrls: ['./resumo.scss'],
 })
-export class ResumoComponent {}
+export class ResumoComponent {
+  verMais() {
+    const mais = document.getElementById('mais');
+    const verMais = document.getElementById('ver-mais');
+    const verMenos = document.getElementById('ver-menos');
+
+    if (mais != undefined) {
+      mais.style.display = 'inline';
+    }
+    if (verMais != undefined) {
+      verMais.style.display = 'none';
+    }
+    if (verMenos != undefined) {
+      verMenos.style.display = 'inline';
+    }
+  }
+  verMenos() {
+    const mais = document.getElementById('mais');
+    const verMais = document.getElementById('ver-mais');
+    const verMenos = document.getElementById('ver-menos');
+    if (mais != undefined) {
+      mais.style.display = 'none';
+    }
+    if (verMais != undefined) {
+      verMais.style.display = 'inline';
+    }
+    if (verMenos != undefined) {
+      verMenos.style.display = 'none';
+    }
+  }
+}
