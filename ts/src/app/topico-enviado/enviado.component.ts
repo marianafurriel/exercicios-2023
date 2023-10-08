dc7eca2e1350ee72d4d5c16eeb7fc075fd8bfe67
Mariana Furriel <marianafurrielss@gmail.com>
2023-10-08 20:14:55 -0300

    feat: criar novo topico adicionado

--- ts/src/app/topico-enviado/enviado.component.ts
+++ ts/src/app/topico-enviado/enviado.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './enviado.component.html',
   styleUrls: ['./enviado.scss'],
 })
-export class EnviadoComponent {}
+export class EnviadoComponent {
+  criarTopico() {
+    const componentEnviado = document.querySelector(
+      'app-enviado'
+    ) as HTMLElement;
+    const componentFormulario = document.querySelector(
+      'app-formulario'
+    ) as HTMLElement;
+    if (componentFormulario != undefined) {
+      componentFormulario.style.display = 'block';
+      componentEnviado.style.display = 'none';
+    }
+  }
+}
