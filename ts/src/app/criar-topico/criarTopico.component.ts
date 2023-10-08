ef3d6ea90fb43c1133540d11298a234c8d5990ae
Mariana Furriel <marianafurrielss@gmail.com>
2023-10-08 18:11:26 -0300

    refactor: criar topico separado em componente

---
+++ ts/src/app/criar-topico/criarTopico.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar',
+  templateUrl: './criar.component.html',
+  styleUrls: ['./criar.scss'],
+})
+export class CriarComponent {
+  criarTopico() {}
+}
