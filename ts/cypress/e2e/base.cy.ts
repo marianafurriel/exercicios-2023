d213f8770e162ec624ef1fbf5297a32fcdf7b9e1
Mariana Furriel <marianafurrielss@gmail.com>
2023-10-10 18:33:23 -0300

    fix: pasta cypress resetada

--- ts/cypress/e2e/base.cy.ts
+++ ts/cypress/e2e/base.cy.ts
@@ -4,9 +4,7 @@ describe('checa elementos básicos', () => {
   });
 
   it('titulo do trabalho existe', () => {
-    cy.get('h2').contains(
-      'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP'
-    );
+    cy.get('h2').contains('Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP');
     cy.compareSnapshot('Trabalho - Base');
   });
 
@@ -27,22 +25,18 @@ describe('checa elementos básicos', () => {
 
   it('clicar em `criar tópico` exibe o formulário', () => {
     cy.get('.btn-create-topic').click();
-    cy.get('button').contains('Enviar');
+    cy.get('button').contains('Enviar')
     cy.get('form').contains('Assunto');
     cy.get('form').contains('Conteúdo');
-    cy.get('input.subject')
-      .invoke('attr', 'placeholder')
-      .should(
-        'contain',
-        'Defina um tópico sucinto para notificar os autores...'
-      );
+    cy.get('input.subject').invoke('attr', 'placeholder').should('contain', 'Defina um tópico sucinto para notificar os autores...');
     cy.compareSnapshot('Trabalho - Criando novo topico');
   });
 
   it('enviar o formulário exibe mensagem de sucesso', () => {
     cy.get('.btn-create-topic').click();
-    cy.get('.enviar-topico').click();
+    cy.get('button').click();
     cy.get('body').contains('Aguardando feedback dos autores');
     cy.compareSnapshot('Trabalho - Topico enviado');
   });
-});
+
+})
